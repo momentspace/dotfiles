a4e8bcad2d39f9d440e9a9694fafe2dd7e5b8266
momentspace <service@starferry.net>
2014-10-04 10:46:11 +0900

    zshrcを追加

--- configure.sh
+++ configure.sh
@@ -5,4 +5,5 @@ ln -s ~/dotfiles/bashrc ~/.bashrc
 ln -s ~/dotfiles/bash_profile ~/.bash_profile
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/dotfiles/gitignore_global ~/.gitignore_global
+ln -s ~/dotfiles/zshrc ~/.zshrc
 
