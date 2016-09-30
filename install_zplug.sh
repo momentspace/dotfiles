1c6ce6e0e1ab15c826413f5da54ed1770d5c7307
momentspace <momentspace@gmail.com>
2016-09-30 11:19:55 +0900

    fix a few mistake

--- install_zplug.sh
+++ install_zplug.sh
@@ -2,5 +2,5 @@ cd ~
 git clone https://github.com/b4b4r07/zplug ~/.zplug
 echo "source ~/.zplug/init.zsh" > ~/.zshrc
 echo 'zplug "sorin-ionescu/prezto"' >> ~/.zshrc
-ln -s $HOME/.zplug/repos/sorin-ionescu/prezto ~/.zprezto
+ln -s ~/.zplug/repos/sorin-ionescu/prezto ~/.zprezto
 echo "restart zsh."
